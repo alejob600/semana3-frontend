cb2b697ec17630980a60276edc47e61237aaf550
colombiatic <alejob600@gmail.com>
2020-12-13 21:44:54 -0500

    principal

---
+++ src/views/login.vue
@@ -0,0 +1,15 @@
+<template>
+ <div class="login">
+     <loginComponent />
+ </div>
+</template>
+<script>
+// @ is an alias to /src
+import loginComponent from "@/components/auth/login.vue";
+export default {
+     name: "login",
+     components: {
+        loginComponent
+    }
+};
+</script>
